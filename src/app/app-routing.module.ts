938fedd8bfc26e30c209ba7179d3d7912977569f
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-25 21:18:07 +0700

    Learn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { MenupageComponent } from './pages/menupage/menupage.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'menu',component:MenuComponent},
+  {path:'menu/:id',component:MenupageComponent},
   {path:'contact',component:ContactComponent}
 ];
 
